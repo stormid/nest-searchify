67e6162ac686fa0273f03ac589dabede7f48fb63
philo <phil@stormid.com>
2015-10-28 15:28:50 +0000

    re #6 - fixes .Empty() using the delimiter as value, also adds tests for creation of FilterFiled via the static .Create() method

--- src/Nest.Searchify/FilterField.cs
+++ src/Nest.Searchify/FilterField.cs
@@ -1,4 +1,5 @@
 using System;
+using Nest.Searchify.Extensions;
 
 namespace Nest.Searchify
 {
@@ -9,17 +10,26 @@ namespace Nest.Searchify
 		public static FilterField Empty(string text = "N/A", string delimiter = DefaultDelimiter)
 		{
 			if (string.IsNullOrWhiteSpace(text)) text = "N/A";
-			return Create(text, delimiter);
+			return Create(text, text.ToUrl(), delimiter);
 		}
 
 		public static FilterField Create(Enum value, string delimiter = DefaultDelimiter)
 		{
 			return Create(value.ToString("G"), value.ToString("D"), delimiter);
 		}
+        public static FilterField CreateWithCustomDelimiter(string text, string delimiter)
+        {
+            return CreateWithCustomDelimiter(text, text.ToUrl(), delimiter);
+        }
 
-		public static FilterField Create(string text)
+        public static FilterField CreateWithCustomDelimiter(string text, string value, string delimiter)
+        {
+            return Create(text, value, delimiter);
+        }
+
+        public static FilterField Create(string text)
 		{
-			return Create(text, text.Replace(" ", "-").ToLowerInvariant());
+			return Create(text, text.ToUrl());
 		}
 
 		public static FilterField Create(string text, string value, string delimiter = DefaultDelimiter)
